e9c15801fbabbdd3e8546ac04d064c28569f6d19
MrF <aabdelrahman.osama.099@gmail.com>
2025-05-05 04:12:24 +0300

    change

--- main.py
+++ main.py
@@ -114,3 +114,7 @@ async def read_root():
     return {"message": "Welcome to the Link API"}
 
 
+if __name__ == "__main__":
+    import os
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
